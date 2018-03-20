5973b9dd9d7086b4614239f83608de5855a44367
Cainã Frei <caina.frei@gmail.com>
2018-03-20 10:56:18 -0300

    Implementado cadastro de descontos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320134917) do
+ActiveRecord::Schema.define(version: 20180320135247) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "country"
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 20180320134917) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_clients_on_user_id"
+  end
+
+  create_table "discounts", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "value"
+    t.integer "kind"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
